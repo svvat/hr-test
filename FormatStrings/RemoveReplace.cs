a99f4db4f0c39959407f9e9f60f74570842fb400
steve wates <swates@mobifed.com>
2020-05-17 17:40:23 +0100

    Tidy

--- FormatStrings/RemoveReplace.cs
+++ FormatStrings/RemoveReplace.cs
@@ -4,6 +4,13 @@ using System.Text;
 
 namespace FormatStrings
 {
+    // task 
+    /// <summary>
+    /// 1 Set the quantity variable to the value you extract from between the <span> and </span> tags.
+    /// 2 Set the output variable to the value of input, then remove the<div> and</div> tags.
+    /// 3 Replace the HTML character &trade; with &reg; in the output variable.
+    /// 
+    /// </summary>
     public class RemoveReplace
     {
         private static string getXML(string input, string key)
@@ -17,7 +24,10 @@ namespace FormatStrings
             const string input = "<div><h2>Widgets &trade;</h2><span>5000</span></div>";
 
             string quantity = getXML(input, "span");
-            string output = input.Replace("trade", "reg");
+            string output = input
+                .Replace("trade", "reg")
+                .Replace("<div>", "")
+                .Replace("</div>", "");
 
             Console.WriteLine($"Quantity: {quantity}");
             Console.WriteLine(output);
