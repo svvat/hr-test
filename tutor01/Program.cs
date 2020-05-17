4ed28f78248dd3bec03bbf5ee075d0063971f808
steve wates <swates@mobifed.com>
2020-05-17 16:22:38 +0100

    Add project files.

---
+++ tutor01/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace tutor01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string customerName = "Mr. Jones";
+
+            string currentProduct = "Magic Yield";
+            int currentShares = 2975000;
+            decimal currentReturn = 0.1275m;
+            decimal currentProfit = 55000000.0m;
+
+            string newProduct = "Glorious Future";
+            decimal newReturn = 0.13125m;
+            decimal newProfit = 63000000.0m;
+
+            // Your logic here
+            Console.WriteLine($"Dear {customerName},");
+            Console.WriteLine($"As a customer of our {currentProduct} offering we are excited to tell you about a new financial product that would dramatically increase your return.\n");
+            Console.WriteLine($"Currently, you own {currentShares:N} shares at a return of {currentReturn:P2}.\n");
+            Console.WriteLine($"Our new product, {newProduct} offers a return of {newReturn:P2}.  Given your current volume, your potential profit would be {newProfit:C}.\n");
+
+            Console.WriteLine("Here's a quick comparison:\n");
+
+            string comparrisonMessage = "";
+
+            comparrisonMessage = currentProduct.PadRight(20);
+            comparrisonMessage += $"{currentReturn:P2}";
+
+            Console.WriteLine(comparrisonMessage);
+
+            Console.ReadKey();
+        }
+    }
+}
