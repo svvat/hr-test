529147c248ecf7652d54f4f5626da11c19c7a03e
steve wates <swates@mobifed.com>
2020-05-17 17:36:27 +0100

    SubStrings

---
+++ FormatStrings/RemoveReplace.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FormatStrings
+{
+    public class RemoveReplace
+    {
+        private static string getXML(string input, string key)
+        {
+            int start = input.IndexOf($"<{key}>") + key.Length + 2;
+            int end = input.IndexOf($"</{key}>");
+            return input.Substring(start, end - start);
+        }
+        public static void execute()
+        {
+            const string input = "<div><h2>Widgets &trade;</h2><span>5000</span></div>";
+
+            string quantity = getXML(input, "span");
+            string output = input.Replace("trade", "reg");
+
+            Console.WriteLine($"Quantity: {quantity}");
+            Console.WriteLine(output);
+
+            Console.ReadKey();
+        }
+    }
+}
